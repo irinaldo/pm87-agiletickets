66285a883abe7402792663a52fad5dffbcd2ce94
Irinaldo <irinaldo.sales@gmail.com>
2014-06-05 19:22:29 -0300

    refactoring...

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -7,42 +7,34 @@ import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
 public class CalculadoraDePrecos {
 
-	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
+	public static BigDecimal calcularPrecoDoIngresso(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
-		
-		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
-			//quando estiver acabando os ingressos... 
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
+		preco = sessao.getPreco();
+		if(((sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW))) 
+			&& (getPorcentagemDeIngressosDisponiveis(sessao) <= 0.05)) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			} else {
-				preco = sessao.getPreco();
-			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
+		} else if((sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) || (sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA))){
+			if(getPorcentagemDeIngressosDisponiveis(sessao) <= 0.50) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
 			}
-			
 			if(sessao.getDuracaoEmMinutos() > 60){
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
-			}
-
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
-		}  else {
-			//nao aplica aumento para teatro (quem vai é pobretão)
-			preco = sessao.getPreco();
 		} 
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
+	}
+
+	private static double getPorcentagemDeIngressosDisponiveis(Sessao sessao) {
+		return getIngressosDisponiveis(sessao) / getTotalDeIngressos(sessao);
+	}
+
+	private static double getTotalDeIngressos(Sessao sessao) {
+		return sessao.getTotalIngressos().doubleValue();
+	}
+
+	private static int getIngressosDisponiveis(Sessao sessao) {
+		return sessao.getTotalIngressos() - sessao.getIngressosReservados();
 	}
 
 }
--- src/main/java/br/com/caelum/agiletickets/models/Sessao.java
+++ src/main/java/br/com/caelum/agiletickets/models/Sessao.java
@@ -97,11 +97,8 @@ public class Sessao {
 		this.ingressosReservados += numeroDeIngressos;
 	}
 
-	public boolean podeReservar(Integer numeroDeIngressos) {
-		int sobraram = getIngressosDisponiveis() - numeroDeIngressos;
-        boolean naoTemEspaco = sobraram < 0;
-
-        return !naoTemEspaco;
+	public boolean podeReservar(Integer nroDeIngressos) {
+        return getIngressosDisponiveis() >= nroDeIngressos;
 	}
 
 	public void setPreco(BigDecimal preco) {
